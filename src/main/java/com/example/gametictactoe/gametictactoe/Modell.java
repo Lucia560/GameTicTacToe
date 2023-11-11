e9fcbd72c9e7c963bcbf377a681df4ef32613734
Lucia Predescu <lucia.predescu@iths.se>
2023-11-11 13:29:25 +0100

    update 2

--- src/main/java/com/example/gametictactoe/gametictactoe/Modell.java
+++ src/main/java/com/example/gametictactoe/gametictactoe/Modell.java
@@ -1,20 +1,72 @@
 package com.example.gametictactoe.gametictactoe;
 
 public class Modell {
-    private char[][] board;      // The game board
-    private char player1;  // Current player ('X' or 'O')
-    private int playerScore;     // Player's score
-    private int computerScore;   // Computer's score
+    private char[][] board;
+    private char player1;
+    private int playerScore;
+    private int computerScore;
 
     public Modell() {
-        // Initialize the game board, player1, playerScore, and computerScore
+        board = new char[3][3];
+        player1 = 'X';
+        playerScore = 0;
+        computerScore = 0;
+
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = ' ';
+            }
+        }
+
+    }
+    public boolean makeMove(int row, int col) {
+        if (row < 0 || row >= 3 || col < 0 || col >= 3 || board[row][col] != ' ') {
+            return false;
+        }
+        board[row][col] = player1;
+
+        if (checkForWinner(row, col)) {
+            if (player1 == 'X') {
+                playerScore++;
+            } else {
+                computerScore++;
+            }
+            return true;
+        }
+
+
+        player1 = (player1 == 'X') ? 'O' : 'X';
+        return true;
     }
 
-    public boolean makeMove(int row, int col) {
-        // Make a move on the board for the current player
-        // Update currentPlayer and check for a winner
-        return false; // Return true if the move is valid, false otherwise
+
+    private boolean checkForWinner(int row, int col) {
+        char currentPlayer = board[row][col];
+
+        if (board[row][0] == currentPlayer && board[row][1] == currentPlayer && board[row][2] == currentPlayer) {
+            return true; //row
+        }
+        if (board[0][col] == currentPlayer && board[1][col] == currentPlayer && board[2][col] == currentPlayer) {
+            return true; //column
+        }
+        if (row == col) {
+            if (board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) {
+                return true; // diagonal
+            }
+        }
+
+        if (row + col == 2) {
+            if (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer) {
+                return true; // diagonal2
+            }
+        }
+
+        return false;
     }
+
+
+
+
 
     public char getPlayer1layer1() {
         // Get the current player ('X' or 'O')
